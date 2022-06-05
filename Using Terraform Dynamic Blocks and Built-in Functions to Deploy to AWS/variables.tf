71231ecfec481d7c11d1c7a3792300370a12a263
Sumedh Vartak <67580847+sumedh004@users.noreply.github.com>
2022-06-05 17:01:59 +0530

    lab 6

---
+++ Using Terraform Dynamic Blocks and Built-in Functions to Deploy to AWS/variables.tf
@@ -0,0 +1,26 @@
+variable "rules" {
+  type = list(object({
+    port        = number
+    proto       = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      port        = 80
+      proto       = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      port        = 22
+      proto       = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      port        = 3333
+      proto       = "tcp"
+      cidr_blocks = ["1.2.3.4/32"]
+    }
+  ]
+
+
+}
